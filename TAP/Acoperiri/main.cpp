0f3e633452eca80a187257757c8f933fe027c8f4
ana <ghiutaanamaria@yahoo.com>
2017-11-08 16:27:26 +0200

    tema web

---
+++ TAP/Acoperiri/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <utility>
+#include<vector>
+#include<fstream>
+#include<algorithm>
+using namespace std;
+ifstream f("fisier.in");
+bool myfunction (pair<int,int> i, pair<int,int> j) { return (i.first<j.first); }
+int main()
+{
+    vector< pair <int,int> > interval;
+    vector< pair <int,int> > solutie;
+    int a,b,n,x,y;
+    //cout<<"a:";cin>>a;
+    //cout<<"b:";cin>>b;
+    //cout<<"n:";cin>>n;
+    f>>a>>b>>n;
+    for(int i=0;i<n;i++){
+        f>>x>>y;
+        interval.push_back(make_pair(x,y));
+    }
+
+    sort(interval.begin(),interval.end(),myfunction);
+//    for(int i=0;i<interval.size();i++)
+//        cout<<interval[i].first<<" "<<interval[i].second<<endl;
+    int var=a;
+    for(int i=0;i<n;i++){
+        if(interval[i].first<=var && interval[i].second>=var){
+            solutie.push_back(interval[i]);
+            var=interval[i].second;
+            //cout<<var;
+            break;
+        }
+    }
+    int i=1;
+
+    for(int i=1;(i<n)&& solutie[solutie.size()-1].second<b;i++){
+
+        if(interval[i].first<=var && interval[i].second>solutie[solutie.size()-1].second   && interval[i+1].second<interval[i].second){
+            solutie.push_back(interval[i]);
+            var=interval[i].second;
+        }
+
+    }
+
+
+    if((solutie.size()==1 && solutie[solutie.size()-1].second<b)||((solutie.size()==0))) cout<<"-1";
+    else {
+        for(int i=0;i<solutie.size();i++){
+        cout<<solutie[i].first<<" "<<solutie[i].second<<endl;
+        }
+    }
+    return 0;
+}
